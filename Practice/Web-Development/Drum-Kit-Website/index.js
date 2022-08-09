588eba8c02b281bd0d5edbe9fb557ea697dc3fd7
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-08-09 20:25:14 +0530

    Add files via upload

---
+++ Practice/Web-Development/Drum-Kit-Website/index.js
@@ -0,0 +1,39 @@
+var drumButtons = document.querySelectorAll(".drum");
+
+// Add button press event listener to all drum sounds
+for (var i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener("click", function () {playSound(this.innerHTML)});
+}
+
+// Add keyboard key-down event listener to all drum sounds
+document.addEventListener("keydown", function(event) {playSound(event.key)});
+
+function playSound(buttonLetter) {
+    switch (buttonLetter) {
+        case 'w':
+            var soundName = "wooden-clave"; break;
+        case 'a':
+            var soundName = "hi-fx"; break;
+        case 's':
+            var soundName = "glass-break-fx"; break;
+        case 'd':
+            var soundName = "o-hi-hat"; break;
+        case 'j':
+            var soundName = "snare"; break;
+        case 'k':
+            var soundName = "c-hi-hat"; break;
+        case 'l':
+            var soundName = "kick-drum"; break;
+
+        default:
+            console.log(buttonLetter);
+    }
+    playButtonSound(soundName);
+}
+
+function playButtonSound(soundName) {
+    var audio = new Audio("sounds/" + soundName + ".mp3");
+    audio.play();
+}
+
+
--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Implementations and experiments in computer science.
 
 | Location | Contents |
 |---|---|
-| [Practice/Web-Development](Practice/Web-Development/) | Browser examples: dice. |
+| [Practice/Web-Development](Practice/Web-Development/) | Browser examples: dice, drum-kit. |
 
 Runtime requirements and validation vary by directory.
